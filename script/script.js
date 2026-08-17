395108ff8144c28ee29c58d0a3e4bdb78ee146b2
suthee-2hats <sutheerth@2hatslogic.com>
2026-08-17 16:13:22 +0530

    password validation completed

--- script/script.js
+++ script/script.js
@@ -6,6 +6,10 @@ const email = document.querySelector("#email");
 
 const age = document.querySelector("#age");
 
+const password = document.querySelector("#password");
+
+const conformPassword = document.querySelector("#confirm-password");
+
 
 //adding event listeners ...
 email.addEventListener("blur", () => {
@@ -17,10 +21,21 @@ userName.addEventListener("blur", () => {
  })
 
  age.addEventListener("blur", () => {
-    console.log("age emailValidation");
+   
     ageValidation(age);
  })
 
+ password.addEventListener("blur", () => {
+   
+    passwordValidation(password);
+ })
+
+ conformPassword.addEventListener("blur", () => {
+    console.log("password validation");
+    conformPasswordValidation(conformPassword,password);
+ })
+
+ 
 
  
 
@@ -86,7 +101,46 @@ function ageValidation(age){
 
 }
 
+//function for passwords 
 
+function passwordValidation(password){
+    const value = password.value;
+    const  errorDisplay = document.querySelector("#password-error");
+
+
+    if(value.length < 8){
+        console.log("it is less than 8 charecter");
+        showErrorInInput(errorDisplay,"password should contain atleast 8 charecter !!");
+        return;
+    }
+    if(!/[A-Z]/.test(value)){
+        showErrorInInput(errorDisplay,"password should contain atleast one  upper class letter !!");
+        return;
+    }
+    if(!/[a-z]/.test(value)){
+        showErrorInInput(errorDisplay,"password should contain atleast one  lower class letter!!");
+        return;
+    }
+    if(!/[0-9]/.test(value)){
+        showErrorInInput(errorDisplay,"password should contain atleast one  numerical value !!");
+        return;
+    }
+    showErrorInInput(errorDisplay,"");
+
+}
+
+function conformPasswordValidation(conformPassword,password){
+    const original = password;
+    const test = conformPassword;
+    const  errorDisplay = document.querySelector("#confirm-password-error");
+
+    if(original.value != test.value){
+        showErrorInInput(errorDisplay,"should be same as before !!");
+        return;
+    }
+    showErrorInInput(errorDisplay,"");
+
+}
 
 
 //common error display 
