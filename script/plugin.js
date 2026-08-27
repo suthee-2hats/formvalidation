2908e2dc3eb038e0d9bf227c68e7a9a79f508e87
suthee-2hats <sutheerth@2hatslogic.com>
2026-08-27 12:56:33 +0530

    added formvalidation for text data in plugin

--- script/plugin.js
+++ script/plugin.js
@@ -20,7 +20,42 @@ export class formvalidation {
             console.log("Blurred element:", event.target);
         
             console.log("Input type:", event.target.type);
+
+            if(event.target.type === "text"){
+                this.textValidation(event.target);
+            }
         
         }, true);
     }
+    textValidation(input_field){
+        const value = input_field;
+        const errorDisplay = input_field.nextElementSibling;
+    
+        const pattern = /^[A-Za-z]+$/;
+        value.pattern = pattern;
+       if(value.validity.valueMissing){
+          
+            this.showErrorInInput(errorDisplay,"name can't be blank !!");
+            return false;
+    
+        }
+        if(value.validity.patternMissmatch)
+        {
+           
+            this.showErrorInInput(errorDisplay,"enter a valid username !!");
+            return false;
+        }
+        this.showErrorInInput(errorDisplay,"");
+        
+    
+       return true;
+
+       
+    }
+    showErrorInInput(displayError,msg){
+
+        displayError.innerHTML = msg;
+      
+    
+    } 
 }
